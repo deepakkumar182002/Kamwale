371355db1b926049fe731f7f1329a01b4645d0bb
Deepak kumar <kumar041232@gmail.com>
2025-02-15 10:16:21 +0530

    add create post feature. and install uploadthing platform for upload files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { Toaster } from "sonner";
+import AuthProvider from "@/components/AuthProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,7 +32,19 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+             <AuthProvider>
+            <NextSSRPlugin
+              /**
+               * The `extractRouterConfig` will extract **only** the route configs
+               * from the router to prevent additional information from being
+               * leaked to the client. The data passed to the client is the same
+               * as if you were to fetch `/api/uploadthing` directly.
+               */
+              routerConfig={extractRouterConfig(ourFileRouter)}
+            />
             {children}
+            <Toaster richColors />
+          </AuthProvider>
           </ThemeProvider>
         </body>
       </html>
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
-
-
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma:PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
-const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma
+const prisma = global.prisma || new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
